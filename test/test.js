ff9757ffb17407c3f2d60efe1a216fb6abf55c93
Miquel A. Cabot <miquel.cabot@gmail.com>
2020-01-13 21:27:56 +0100

    Test smart contract

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const ElGamal = require('elgamal');
+const bigInt = require("big-integer");
 
 const compiledFactoryPath = '../contracts/build/ConfidentialMultipartyRegisteredEDeliveryWithoutTTPFactory.json';
 const compiledDeliveryPath = '../contracts/build/ConfidentialMultipartyRegisteredEDeliveryWithoutTTP.json';
@@ -11,21 +13,73 @@ const compiledDelivery = require(compiledDeliveryPath);
 let factoryContract;
 let deliveryContract;
 let deliveryContractAddress;
-let accounts;
+let accounts; 
+
+const NUMBER_BITS = 512;
+const MESSAGE = "Hola, com va tot?";
+
+let elgamal, p, g, q, xa, ya, r, messageSent, m1, m2;
+let c, s, xb, yb, z1, z2;
+let w;
 
 // To prevent warning "MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 data listeners added. Use emitter.setMaxListeners() to increase limit"
 require('events').EventEmitter.defaultMaxListeners = 0;
 
 beforeEach(async () => {
+  // VARIABLES FOR CREATE()
+  // Generation of p, g, q of ElGamal algorithm
+  elgamal = await ElGamal.default.generateAsync(NUMBER_BITS);
+  p = bigInt(elgamal.p.toString());
+  g = bigInt(elgamal.g.toString());
+  q = p.minus(1).divide(2);
+
+  // Generation of xa, ya, private and public keys of A
+  // ya = g^xa mod p
+  xa = bigInt.randBetween(2, q.minus(1));
+  ya = g.modPow(xa, p);
+  
+  // Generation of random number r
+  r = bigInt.randBetween(2, q.minus(1));
+  
+  let messageSentBuffer = Buffer.from(MESSAGE, 'utf8');
+  messageSent = bigInt(messageSentBuffer.toString('hex'), 16);
+
+  // Generation of M1 = g^r mod p
+  m1 = g.modPow(r, p);
+
+  // Generation of M2 = m·ya^r mod p
+  m2 = messageSent.multiply(ya.modPow(r, p));
+
+  // VARIABLES FOR ACCEPT()
+  // Generation of challenge number c
+  c = bigInt.randBetween(2, q.minus(1));      // Pot ser mes curt, meitat de bits
+  
+  // Generation of random number s
+  s = bigInt.randBetween(2, q.minus(1));
+
+  // Generation of xb, yb, private and public keys of B
+  // yb = g^xb mod p
+  xb = bigInt.randBetween(2, q.minus(1));
+  yb = g.modPow(xb, p);
+
+  // Generation of z1 = g^s mod p
+  z1 = g.modPow(s, p);
+  // Generation of z2 = xb·ya^s mod p
+  z2 = xb.multiply(ya.modPow(s, p));
+
+  // VARIABLES FOR FINISH()
+  //const w = r.add(c.mod(p).multiply(xb.mod(p)).mod(p));
+  w =  r.add(c.multiply(xb.mod(p)));
+
   accounts = await web3.eth.getAccounts();
 
   factoryContract = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode, arguments: [] })
-    .send({ from: accounts[0], gas: '3000000' });
+    .send({ from: accounts[0], gas: '6000000' });
 
   await factoryContract.methods
-    .createDelivery([accounts[1],accounts[2]], web3.utils.keccak256("Test message"), 600, 1200)
-    .send({ from: accounts[0], gas: '3000000', value: '100' });
+    .createDelivery([accounts[1],accounts[2]], "0x"+m1.toString(16), "0x"+m2.toString(16), "0x"+ya.toString(16), "0x"+g.toString(16), "0x"+p.toString(16), 600, 1200)
+    .send({ from: accounts[0], gas: '6000000', value: '100' });
 
   const addresses = await factoryContract.methods.getDeliveries().call();
   deliveryContractAddress = addresses[0];
@@ -39,14 +93,11 @@ describe('Certified eDelivery Contract', () => {
     assert.ok(deliveryContract.options.address);
   });
 
-  it("state is created and has a hash message", async function () {
-    var messageHash = await deliveryContract.methods.messageHash().call();
-    assert.equal(messageHash, web3.utils.keccak256("Test message"));
-  });
-
   it("non receivers can't accept delivery", async function() {
     try { 
-      await deliveryContract.methods.accept().send({ from: accounts[3] });
+      await deliveryContract.methods
+        .accept("0x"+z1.toString(16), "0x"+z2.toString(16), "0x"+yb.toString(16), "0x"+c.toString(16))
+        .send({ from: accounts[3], gas: '6000000' });
       assert(false);
     } catch (err) {
       assert(err);
@@ -54,16 +105,24 @@ describe('Certified eDelivery Contract', () => {
   });
 
   it("receiver can accept delivery", async function() {
-    await deliveryContract.methods.accept().send({ from: accounts[1] });
-    var state = await deliveryContract.methods.getState(accounts[1]).call();
+    await deliveryContract.methods
+      .accept("0x"+z1.toString(16), "0x"+z2.toString(16), "0x"+yb.toString(16), "0x"+c.toString(16))
+      .send({ from: accounts[1], gas: '6000000' });
+    let state = await deliveryContract.methods.getState(accounts[1]).call();
     assert.equal(state, "accepted");
   });
 
   it("non sender can't finish delivery", async function() {
-    await deliveryContract.methods.accept().send({ from: accounts[1] });
-    await deliveryContract.methods.accept().send({ from: accounts[2] });
+    await deliveryContract.methods
+      .accept("0x"+z1.toString(16), "0x"+z2.toString(16), "0x"+yb.toString(16), "0x"+c.toString(16))
+      .send({ from: accounts[1], gas: '6000000' });
+    await deliveryContract.methods
+      .accept("0x"+z1.toString(16), "0x"+z2.toString(16), "0x"+yb.toString(16), "0x"+c.toString(16))
+      .send({ from: accounts[2], gas: '6000000' });
     try { 
-      await deliveryContract.methods.finish("Test message").send({ from: accounts[3] });
+      await deliveryContract.methods
+        .finish(accounts[1], "0x"+w.toString(16), "0x"+r.toString(16))
+        .send({ from: accounts[3], gas: '6000000' });
       assert(false);
     } catch (err) {
       assert(err);
@@ -71,12 +130,37 @@ describe('Certified eDelivery Contract', () => {
   });
 
   it("sender can finish delivery", async function() {
-    await deliveryContract.methods.accept().send({ from: accounts[1] });
-    await deliveryContract.methods.accept().send({ from: accounts[2] });
-    await deliveryContract.methods.finish("Test message").send({ from: accounts[0] });
-    var message = await deliveryContract.methods.message().call();
-    var state = await deliveryContract.methods.getState(accounts[1]).call();
-    assert.equal(message, "Test message");
+    await deliveryContract.methods
+      .accept("0x"+z1.toString(16), "0x"+z2.toString(16), "0x"+yb.toString(16), "0x"+c.toString(16))
+      .send({ from: accounts[1], gas: '6000000' });
+    await deliveryContract.methods
+      .accept("0x"+z1.toString(16), "0x"+z2.toString(16), "0x"+yb.toString(16), "0x"+c.toString(16))
+      .send({ from: accounts[2], gas: '6000000' });
+    await deliveryContract.methods
+      .finish(accounts[1], "0x"+w.toString(16), "0x"+r.toString(16))
+      .send({ from: accounts[0], gas: '6000000' });
+    let state = await deliveryContract.methods.getState(accounts[1]).call();
     assert.equal(state, "finished");
+  });
+
+  it("received message is correct", async function() {
+    await deliveryContract.methods
+      .accept("0x"+z1.toString(16), "0x"+z2.toString(16), "0x"+yb.toString(16), "0x"+c.toString(16))
+      .send({ from: accounts[1], gas: '6000000' });
+    await deliveryContract.methods
+      .accept("0x"+z1.toString(16), "0x"+z2.toString(16), "0x"+yb.toString(16), "0x"+c.toString(16))
+      .send({ from: accounts[2], gas: '6000000' });
+    await deliveryContract.methods
+      .finish(accounts[1], "0x"+w.toString(16), "0x"+r.toString(16))
+      .send({ from: accounts[0], gas: '6000000' });
+
+    let _m2 = bigInt((await deliveryContract.methods.m2().call()).substr(2), 16);
+    let _ya = bigInt((await deliveryContract.methods.ya().call()).substr(2), 16);
+    let _p = bigInt((await deliveryContract.methods.p().call()).substr(2), 16);
+    let _r = r;  // Pendent
+
+    const messageReceived = _m2.divide(_ya.modPow(_r, _p));
+    const messageReceivedBuffer = Buffer.from(messageReceived.toString(16), 'hex');
+    assert.equal(messageReceivedBuffer, MESSAGE);
   });
 });
