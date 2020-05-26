a1c99b8e0efce557108239bc3d52efa76039f299
Miquel A. Cabot <miquel.cabot@gmail.com>
2020-05-26 23:51:20 +0200

    Store address in compiled contract js

--- deploy.js
+++ deploy.js
@@ -1,13 +1,14 @@
+const path = require("path");
+const fs = require("fs-extra"); // fs with extra functions
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const fs = require("fs");
 
 const compiledFactoryPath = './contracts/build/ConfidentialMultipartyRegisteredEDeliveryWithoutTTPFactory.json';
 const compiledFactory = require(compiledFactoryPath);
 
 const provider = new HDWalletProvider(
   'tragic square news business dad cricket nurse athlete tide split about ring',
-  'https://rinkeby.infura.io/6Fb0b6c4nUVQBb8qAKcx'
+  'https://rinkeby.infura.io/v3/b2daf36eb4d74aed8ffac330c09dd2ee'
 );
 const web3 = new Web3(provider);
 
@@ -18,9 +19,17 @@ const deploy = async () => {
 
   const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode, arguments: [] })
-    .send({ from: accounts[0], gas: '3000000' });
+    .send({ from: accounts[0], gas: '6000000' });
 
-  fs.writeFileSync('./CONTRACTADDRESS', result.options.address);
+  //fs.writeFileSync('./CONTRACTADDRESS', result.options.address);
+  compiledFactory.address = result.options.address;
+
+  fs.outputJsonSync(
+    path.resolve(__dirname, compiledFactoryPath),
+    compiledFactory,
+    {spaces: 2} // Indent json output with 2 spaces
+  );
+
   console.log('Contract deployed to Rinkeby network, at address ', result.options.address);
 };
 deploy();
